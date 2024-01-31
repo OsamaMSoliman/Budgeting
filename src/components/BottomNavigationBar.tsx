9080712f61e5939ecf97d1101ec82558f539647b
Osama Soliman <Osama.M.Soliman@uni-bonn.de>
2024-01-31 21:58:50 +0100

    fix bottom navbar so it doesn't overlap components if pulled to the end of page

--- src/components/BottomNavigationBar.tsx
+++ src/components/BottomNavigationBar.tsx
@@ -8,8 +8,7 @@ function BottomNavigationBar() {
     const [value, setValue] = useState(0);
 
     return (
-        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={5}>
-
+        <Paper sx={{ position: 'sticky', bottom: 0, left: 0, right: 0 }} elevation={5}>
             <BottomNavigation
                 value={value}
                 onChange={(event, newValue) => {
