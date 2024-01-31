f8993b8f17ffeb3b39f700f1804c00456326082c
Osama Soliman <Osama.M.Soliman@uni-bonn.de>
2024-01-31 22:48:00 +0100

    - Items page - Balance box is large when on the top of the page - Removed Cards route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./components/AppLayout";
-import CardsListPage from "./pages/CardsListPage";
 import Page404 from "./pages/Page404";
 import ItemsPage from "./pages/ItemsPage";
 import ExportImportPage from "./pages/ExportImportPage";
@@ -9,10 +8,9 @@ export default () => (
   <BrowserRouter>
     <Routes>
       <Route element={<AppLayout />}>
-        <Route path="/" element={<ItemsPage/>} />
-        <Route path="/export" element={<ExportImportPage/>} />
-        <Route path="/import" element={<ExportImportPage/>} />
-        <Route path="/cards" element={<CardsListPage />} />
+        <Route path="/" element={<ItemsPage />} />
+        <Route path="/export" element={<ExportImportPage />} />
+        <Route path="/import" element={<ExportImportPage />} />
       </Route>
       <Route path="*" element={<Page404 />} />
     </Routes>
--- src/components/BalanceBox.tsx
+++ src/components/BalanceBox.tsx
@@ -4,11 +4,12 @@ import React from "react";
 interface IBalanceBoxProps {
     budget: number,
     total: number,
+    isLarge?: boolean,
 }
 
-const BalanceBox: React.FC<IBalanceBoxProps> = ({ budget, total }) => (
+const BalanceBox: React.FC<IBalanceBoxProps> = ({ budget, total, isLarge = false }) => (
     <Paper elevation={24} sx={{ p: 1 }}>
-        <Typography align="center">{total} € / {budget} € </Typography>
+        <Typography variant={isLarge ? "h4" : "body1"} align="center">{total} € / {budget} € </Typography>
     </Paper>
 );
 export default BalanceBox;
