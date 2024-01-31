794f44e5d1c7dfc1c1b571dbb6e215e11052a977
Osama Soliman <Osama.M.Soliman@uni-bonn.de>
2024-01-31 21:03:50 +0100

    - BalanceBox takes budget and total - BottomCardDrawer

--- src/components/BalanceBox.tsx
+++ src/components/BalanceBox.tsx
@@ -1,11 +1,14 @@
 import { Paper, Typography } from "@mui/material";
+import React from "react";
 
-function BalanceBox() {
-    return (
-        <Paper elevation={24} sx={{ p: 1 }}>
-            <Typography align="center">Target € / Budget € </Typography>
-        </Paper>
-    );
+interface IBalanceBoxProps {
+    budget: number,
+    total: number,
 }
 
+const BalanceBox: React.FC<IBalanceBoxProps> = ({ budget, total }) => (
+    <Paper elevation={24} sx={{ p: 1 }}>
+        <Typography align="center">{total} € / {budget} € </Typography>
+    </Paper>
+);
 export default BalanceBox;
