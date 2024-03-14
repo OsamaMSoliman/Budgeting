98cf995784a5072b8b0443319eb3516aab73dc9a
Osama Soliman <Osama.M.Soliman@uni-bonn.de>
2024-03-14 19:01:32 +0100

    - divided the store into state variables and actions - added id to the items - fixed upsertItem - fixed deleteItem - added computed value count

--- src/store/useItemStore.ts
+++ src/store/useItemStore.ts
@@ -6,9 +6,21 @@ interface IStoreState {
     items: Array<IItem>;
 }
 
+interface IStoreActions {
+    setTotal: (total: number) => void;
+    setBudget: (budget: number) => void;
+    upsertItem: (item: IItem) => void;
+    deleteItem: (item: IItem) => void;
+    count: () => number;
+    clear: () => void;
+    save: () => void;
+    load: () => void;
+}
+
 export interface IItem {
+    id: number;
     name: string;
-    quantitiy: number;
+    quantity: number;
     price: number;
 }
 
@@ -18,15 +30,23 @@ const initialState: IStoreState = {
     total: 0,
     budget: 0,
     items: [],
-}
+};
 
-export const useItemStore = create<IStoreState>()(
+// const useItemStore: UseBoundStore<StoreApi<IStoreState & IStoreActions>>
+export const useItemStore = create<IStoreState & IStoreActions>()(
     (set, get) => ({
         ...initialState,
         setTotal: (total: number) => set({ total }),
         setBudget: (budget: number) => set({ budget }),
-        upsertItem: (item: IItem) => set({ items: [...get().items, item] }),
-        deleteItem: (item: IItem) => set({ items: get().items.filter(i => i !== item) }),
+        upsertItem: (item: IItem) => {
+            const index = get().items.findIndex(i => i.id === item.id);
+            set({
+                items: index === -1 ? [...get().items, { ...item }]
+                    : [...get().items.slice(0, index), { ...item }, ...get().items.slice(index + 1)]
+            });
+        },
+        deleteItem: (item: IItem) => set({ items: get().items.filter(i => i.id !== item.id) }),
+        count: () => get().items.length,
         clear: () => set(initialState),
         save: () => localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(get())),
         load: () => JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || 'null') ?? initialState,
