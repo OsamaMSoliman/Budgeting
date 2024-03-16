8038f4b0c1afd7d1a93dba4865297826674f18dd
Osama Soliman <Osama.M.Soliman@uni-bonn.de>
2024-03-16 10:30:13 +0100

    fix upsert when adding new item

--- src/store/useItemStore.ts
+++ src/store/useItemStore.ts
@@ -39,8 +39,15 @@ export const useItemStore = create<IStoreState & IStoreActions>()(
                     ...initialState,
                     setTotal: (total: number) => set({ total }),
                     setBudget: (budget: number) => set({ budget }),
-                    // upsertItem: (item: IItem) => get().items[item.id] = item,
-                    upsertItem: (item: IItem) => set(state => state.items[item.id] = item),
+                    // upsertItem: (item: IItem) => get().items[item.id] ? get().items[item.id] = item : get().items.push(item);
+                    upsertItem: (item: IItem) => set(state => {
+                        try {
+                            state.items[item.id] = item
+                        } catch (error) {
+                            state.items.push(item);
+                        }
+                    }),
+                    // TODO: using the index will introduce bugs when deleteItem is being utilized, Solution: maybe use dictionary instead of Array!
                     // deleteItem: (item: IItem) => delete get().items[item.id],
                     deleteItem: (item: IItem) => set(state => delete state.items[item.id]),
                     count: () => get().items.length,
