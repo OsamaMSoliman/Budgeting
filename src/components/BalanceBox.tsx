b0ebddc08066fd70a1befeba1b1cdc9d4b903868
Osama Soliman <Osama.M.Soliman@uni-bonn.de>
2024-03-17 18:44:38 +0100

    set the buget from the balance box

--- src/components/BalanceBox.tsx
+++ src/components/BalanceBox.tsx
@@ -1,18 +1,36 @@
-import { AppBar, Paper, Toolbar, Typography } from "@mui/material";
+import { AppBar, Paper, TextField, Toolbar, Typography } from "@mui/material";
 import { useItemStore } from "../store/useItemStore";
+import { useState } from "react";
 
 export default () => {
-    const { total, budget } = useItemStore();
-
+    const budget = useItemStore(state => state.budget);
+    const setBudget = useItemStore(state => state.setBudget);
+    const total = useItemStore(state => state.total);
     const color = total < budget ? "success" : total == budget ? "warning" : "error";
+    const [edit, setEdit] = useState(budget === 0);
 
-    return (<>
+    const handleKeyDown = (event: React.KeyboardEvent) => {
+        if (event.key === 'Enter') {
+            setBudget(Number((event.target as HTMLInputElement).value));
+            setEdit(false);
+        }
+    };
+
+    return (
         <AppBar position="sticky" elevation={24} color={color}>
             <Toolbar >
-                <Paper sx={{ p: 1, m: 1, flexGrow: 1 }}>
-                    <Typography variant="h4" align="center">{total} € / {budget} € </Typography>
+                <Paper sx={{ p: 1, m: 1, flexGrow: 1 }} onClick={() => setEdit(true)}>
+                    {
+                        edit ?
+                            <TextField label="Budget" type="number" fullWidth
+                                InputProps={{ endAdornment: "€" }} inputProps={{ step: 0.01 }}
+                                onKeyDown={handleKeyDown}
+                            />
+                            :
+                            <Typography variant="h4" align="center">{total} € / {budget} €</Typography>
+                    }
                 </Paper>
             </Toolbar>
-        </AppBar>
-    </>);
+        </AppBar >
+    );
 };
