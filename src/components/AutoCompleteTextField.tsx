cf2e5a18c6a28e7a902afa3fa0c66d98536560d1
Osama Soliman <Osama.M.Soliman@uni-bonn.de>
2024-03-17 17:37:12 +0100

    items suggestions are grouped by the first letter

--- src/components/AutoCompleteTextField.tsx
+++ src/components/AutoCompleteTextField.tsx
@@ -1,4 +1,4 @@
-import { Autocomplete, TextField } from "@mui/material";
+import { Autocomplete, TextField, darken, lighten, styled } from "@mui/material";
 import { useItemStore } from "../store/useItemStore";
 
 interface IProps {
@@ -15,8 +15,30 @@ export default (textFieldProps: IProps) => {
             id="item-name"
             freeSolo
             disableClearable
-            options={items.map((item) => item.name)}
-            renderInput={(params) => <TextField {...params} {...textFieldProps}/>}
+            options={items.map((item) => item.name).sort((a, b) => a[0].localeCompare(b[0]))}
+            groupBy={(option) => option[0].toUpperCase()}
+            renderInput={(params) => <TextField {...params} {...textFieldProps} />}
+            renderGroup={(params) => (
+                <li key={params.key}>
+                    <GroupHeader>{params.group}</GroupHeader>
+                    <GroupItems>{params.children}</GroupItems>
+                </li>
+            )}
         />
     );
 };
+
+const GroupHeader = styled('div')(({ theme }) => ({
+    position: 'sticky',
+    top: '-8px',
+    padding: '4px 10px',
+    color: theme.palette.primary.main,
+    backgroundColor:
+        theme.palette.mode === 'light'
+            ? lighten(theme.palette.primary.light, 0.85)
+            : darken(theme.palette.primary.main, 0.8),
+}));
+
+const GroupItems = styled('ul')({
+    padding: 0,
+});
