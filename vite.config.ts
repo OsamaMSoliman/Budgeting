1b467163712aa243870b7b2790b7e1b325e242b3
Osama Soliman <Osama.M.Soliman@uni-bonn.de>
2024-03-21 10:44:28 +0100

    fix: manifest relative path of images fix: change project name and version

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,45 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/Budgeting/",
+    base: "/CheckOutCheck/",
     plugins: [react(), VitePWA({
-        // registerType: 'prompt',
-        // includeAssets: ['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+        registerType: 'autoUpdate',
+        includeAssets: ["**/*"], //https://adueck.github.io/blog/caching-everything-for-totally-offline-pwa-vite-react/
         manifest: {
-            name: "Budgeting Checkout",
-            start_url: "/",
+            name: "Budgeting Checkout Check",
             display: "standalone",
             icons: [
                 {
-                    "src": "/favicon_io/android-chrome-192x192.png",
+                    "src": "./favicon_io/android-chrome-192x192.png",
                     "sizes": "192x192",
                     "type": "image/png"
                 },
                 {
-                    "src": "/favicon_io/android-chrome-512x512.png",
+                    "src": "./favicon_io/android-chrome-512x512.png",
                     "sizes": "512x512",
                     "type": "image/png"
                 },
                 {
-                    "src": "/favicon_io/apple-touch-icon.png",
+                    "src": "./favicon_io/apple-touch-icon.png",
                     "sizes": "180x180",
                     "type": "image/png"
                 },
                 {
-                    "src": "/favicon_io/favicon-16x16.png",
+                    "src": "./favicon_io/favicon-16x16.png",
                     "sizes": "16x16",
                     "type": "image/png"
                 },
                 {
-                    "src": "/favicon_io/favicon-32x32.png",
+                    "src": "./favicon_io/favicon-32x32.png",
                     "sizes": "32x32",
-                    "type": "image/png"
+                    "type": "image/png",
+                    "purpose": "maskable",
                 }
             ],
-            short_name: "budgeting",
+            short_name: "CheckCheck",
             description: "local groceries list app",
             orientation: 'portrait',
             theme_color: '#ffffff',
